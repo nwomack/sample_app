573e418008e9b135bebcf4029273790d1d72d91c
Nathan Womack <nwomack@gmail.com>
2010-10-15 14:27:29 +0800

    section 6 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
+
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  
+
   root :to => 'pages#home'
 
   # You can have the root of your site routed with "root"
