016760a6b0c5f38ea821a5686eca8ea22fca68ad
Nathan Womack <nwomack@gmail.com>
2010-10-26 15:47:51 +0800

    section 10.3 complete

--- Gemfile
+++ Gemfile
@@ -3,13 +3,15 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.0'
 gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development do
   gem 'annotate-models', '1.0.4'
-  gem 'rspec-rails', '2.0.0'
+  gem 'rspec-rails', '2.0.1'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
 
   def create
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @title = "Sign up"
+  end
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
 
   def show
